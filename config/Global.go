8ea4a3345e3dc96fa297a93cf765a0120c888501
panda30ss <jialiangsong30@163.com>
2022-08-29 16:50:51 +0800

    1

---
+++ config/Global.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	log "core/logManager"
+)
+
+const (
+	Master     = iota // value --> 0
+	Cron              // value --> 1
+	GameServer        // value --> 2
+	Gate              // value --> 3
+	Count             // value --> 3
+)
+
+func GetInstance() *config {
+	once.Do(func() {
+		instance = &config{}
+	})
+	return instance
+}
+
+func Register(role int) {
+	GetInstance().Server.Roles[uint8(role)] = true
+}
+
+func GetModules() map[string]bool {
+	return GetInstance().modules
+}
+
+func GetString(key string) string {
+	defer func() {
+		if e := recover(); e != nil {
+			log.Error(e)
+		}
+	}()
+	return GetInstance().data.MustKey(key).MustString()
+}
+
+func GetInt(key string) int {
+	defer func() {
+		if e := recover(); e != nil {
+			log.Error(e)
+		}
+	}()
+	ret := 0
+	ret = int(GetInstance().data.MustKey(key).MustNumeric())
+	return ret
+}
+
+func GetBool(key string) bool {
+	defer func() {
+		if e := recover(); e != nil {
+			log.Error(e)
+		}
+	}()
+	return GetInstance().data.MustKey(key).MustBool()
+}
